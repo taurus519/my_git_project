7d7ed36cbb5ebbf3c70d6c7d680d4d38cc8da1a8
YI <jew519@gmail.com>
2018-07-10 22:17:12 +0800

    add hello stacy.

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,5 +5,6 @@ int main()
    std::cout << "hello world" << std::endl;
    std::cout << "hello hell"  << std::endl;
    std::cout << "hello willie"  << std::endl;
+   std::cout << "hello stacy"  << std::endl;
 
 }
